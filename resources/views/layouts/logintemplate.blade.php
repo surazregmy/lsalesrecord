bd5cdc3b174881d806747221a3949b407565b748
surazzzzz5 <surazz.regmi123@gmail.com>
2018-03-20 23:35:30 +0545

    Design to login page and Dashboard controller added

---
+++ resources/views/layouts/logintemplate.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V3</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	
+	<link rel="stylesheet" type="text/css" href="{{asset('css/footer/font-awesome.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/footer/material-design-iconic-font.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/footer/animate.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/footer/main.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/footer/util.css')}}">
+
+</head>
+<body>
+	
+	@yield('content')
+	
+
+
+	
+
+
+	<script src="{{asset('js/footer/jquery-3.2.1.min.js')}}"></script>
+	<script src="{{asset('js/footer/bootstrap.min.js')}}"></script>
+	<script src="{{asset('js/footer/main.js')}}"></script>
+	
+</body>
+</html>
